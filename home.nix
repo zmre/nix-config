9a94bca61561b849eeebe37f28ed58d38b4ec315
Patrick Walsh <patrick.walsh@ironcorelabs.com>
2021-11-11 17:34:41 -0700

    Fixed syntax errors and formatted

--- home.nix
+++ home.nix
@@ -33,12 +33,18 @@
   ];
 
   home.file.".p10k.zsh".source = ./home/dotfiles/p10k.zsh;
-  home.file.".config/nvim/lua/options.lua".source = ./home/dotfiles/nvim/lua/options.lua;
-  home.file.".config/nvim/lua/abbreviations.lua".source = ./home/dotfiles/nvim/lua/abbreviations.lua;
-  home.file.".config/nvim/lua/filetypes.lua".source = ./home/dotfiles/nvim/lua/filetypes.lua;
-  home.file.".config/nvim/lua/mappings.lua".source = ./home/dotfiles/nvim/lua/mappings.lua;
-  home.file.".config/nvim/lua/tools.lua".source = ./home/dotfiles/nvim/lua/tools.lua;
-  home.file.".config/nvim/vim/colors.vim".source = ./home/dotfiles/nvim/vim/colors.vim;
+  home.file.".config/nvim/lua/options.lua".source =
+    ./home/dotfiles/nvim/lua/options.lua;
+  home.file.".config/nvim/lua/abbreviations.lua".source =
+    ./home/dotfiles/nvim/lua/abbreviations.lua;
+  home.file.".config/nvim/lua/filetypes.lua".source =
+    ./home/dotfiles/nvim/lua/filetypes.lua;
+  home.file.".config/nvim/lua/mappings.lua".source =
+    ./home/dotfiles/nvim/lua/mappings.lua;
+  home.file.".config/nvim/lua/tools.lua".source =
+    ./home/dotfiles/nvim/lua/tools.lua;
+  home.file.".config/nvim/vim/colors.vim".source =
+    ./home/dotfiles/nvim/vim/colors.vim;
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
@@ -51,28 +57,28 @@
     viAlias = true;
     vimAlias = true;
     extraConfig = ''
-    lua << 
-        require('options').defaults()
-        require('options').gui()
-        require('mappings')
-        require('abbreviations')
-        require('filetypes').config()
-    
+      lua << 
+          require('options').defaults()
+          require('options').gui()
+          require('mappings')
+          require('abbreviations')
+          require('filetypes').config()
+      
     '';
     #extraConfig = builtins.readFile ./home/extraConfig.vim;
     #extraConfig = let
-      #luaRequire = module: builtins.readFile (builtins.toString 
-        #./home/dotfiles + "/${module}.lua");
-      #luaConfig = builtins.concatStringsSep "\n" (map luaRequire [
-        #"options"
-        #"mappings"
-        #"abbreviations"
-        #"filetypes"
-      ##]);
+    #luaRequire = module: builtins.readFile (builtins.toString 
+    #./home/dotfiles + "/${module}.lua");
+    #luaConfig = builtins.concatStringsSep "\n" (map luaRequire [
+    #"options"
+    #"mappings"
+    #"abbreviations"
+    #"filetypes"
+    ##]);
     #in ''
-      #lua << 
-      #${luaConfig}
-      #
+    #lua << 
+    #${luaConfig}
+    #
     #'';
 
     plugins = with pkgs.vimPlugins; [
@@ -110,8 +116,8 @@
     enable = true;
     enableZshIntegration = true;
     tmux.enableShellIntegration = true;
-  }
-  program.gh.enable = true;
+  };
+  programs.gh.enable = true;
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -138,7 +144,8 @@
       "ripgrep"
       "zoxide"
     ];
-    oh-my-zsh.theme = "../../../../../../../../$HOME/.nix-profile/share/zsh-powerlevel10k/powerlevel10k";
+    oh-my-zsh.theme =
+      "../../../../../../../../$HOME/.nix-profile/share/zsh-powerlevel10k/powerlevel10k";
     #oh-my-zsh.theme = "muse";
     shellAliases = {
       ls = "ls --color=auto -F";
@@ -177,31 +184,33 @@
     userName = "Patrick Walsh";
     userEmail = "patrick.walsh@ironcorelabs.com";
     aliases = {
-    tatus = "status";
-	co = "checkout";
-    br = "branch";
-	st = "status -sb";
-	wtf = "!git-wtf";
-	lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --topo-order --date=relative";
-	gl = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --topo-order --date=relative";
-	lp = "log -p";
-	lr = "reflog";
-	ls = "ls-files";
-	dall = "diff";
-	d = "diff --relative";
-	dv = "difftool";
-	df = "diff --relative --name-only";
-	dvf = "difftool --relative --name-only";
-	dfall = "diff --name-only";
-	ds = "diff --relative --name-status";
-	dvs = "difftool --relative --name-status";
-	dsall = "diff --name-status";
-	dvsall = "difftool --name-status";
-	dr = "diff-index --cached --name-only --relative HEAD";
-	di = "diff-index --cached --patch --relative HEAD";
-	dfi = "diff-index --cached --name-only --relative HEAD";
-    subpull = "submodule foreach git pull";
-    subco = "submodule foreach git checkout master";
+      tatus = "status";
+      co = "checkout";
+      br = "branch";
+      st = "status -sb";
+      wtf = "!git-wtf";
+      lg =
+        "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --topo-order --date=relative";
+      gl =
+        "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --topo-order --date=relative";
+      lp = "log -p";
+      lr = "reflog";
+      ls = "ls-files";
+      dall = "diff";
+      d = "diff --relative";
+      dv = "difftool";
+      df = "diff --relative --name-only";
+      dvf = "difftool --relative --name-only";
+      dfall = "diff --name-only";
+      ds = "diff --relative --name-status";
+      dvs = "difftool --relative --name-status";
+      dsall = "diff --name-status";
+      dvsall = "difftool --name-status";
+      dr = "diff-index --cached --name-only --relative HEAD";
+      di = "diff-index --cached --patch --relative HEAD";
+      dfi = "diff-index --cached --name-only --relative HEAD";
+      subpull = "submodule foreach git pull";
+      subco = "submodule foreach git checkout master";
     };
     extraConfig = {
       github.user = "zmre";
@@ -217,264 +226,267 @@
     delta = {
       enable = true;
       options = {
-    syntax-theme = "Monokai Extended";
-    line-numbers = true;
-    navigate = true;
-    side-by-side = true;
+        syntax-theme = "Monokai Extended";
+        line-numbers = true;
+        navigate = true;
+        side-by-side = true;
       };
     };
     ignores = [
-$RECYCLE.BIN/
-*$py.class
-**/*.rs.bk
-**/linux_mysql/*.d
-**/target
-*,cover
-*-journal
-*-session.json
-*.Cache
-*.DS_Store
-*.a
-*.back
-*.backup
-*.bak
-*.cache
-*.class
-*.clbin
-*.compiled.js
-*.d
-*.dSYM
-*.dSYM/
-*.dll
-*.dmp
-*.dump
-*.dylib
-*.elf
-*.exe
-*.hex
-*.idb
-*.kate-swp
-*.kicad_pcb-bak
-*.ko
-*.komodoproject
-*.la
-*.launch
-*.lib
-*.lnk
-*.lo
-*.lock
-*.log
-*.manifest
-*.map
-*.ncb
-*.o
-*.o.d
-*.obj
-*.old
-*.orig
-*.out
-*.pbxuser
-*.pdb
-*.perspectivev3
-*.pid
-*.pid.lock
-*.py[cdo]
-*.py[co]
-*.py[cod]
-*.pyc
-*.pydevproject
-*.pyo
-*.py~
-*.sfd~
-*.so
-*.so.*
-*.sublime*
-*.sublime-*
-*.sublime-workspace
-*.suit
-*.suo
-*.sw*
-*.swn
-*.swo
-*.swp
-*.tlh
-*.tli
-*.tmp
-*.trace
-*.un~
-*.user
-*.vsp
-*.vspscc
-*.xccheckout
-*.xcodeproj/
-*.xcuserstate
-*.xcworkspace
-*/**/.DS_Store
-*_generated.phph
-*~
-*~.DS_STORE
-*~.nib
-.#*
-.*.sw*
-.*.swo
-.*.swp
-.AppleDB
-.AppleDesktop
-.AppleDouble
-.CREATED
-.DS_Store
-.DocumentRevisions-V100
-.LSOverride
-.Python
-.Spotlight-V100
-.TemporaryItems
-.Thumbs.db
-.Trash-*
-.Trashes
-.VolumeIcon.icns
-._*
-.apdisk
-.build/
-.buildpath
-.builds
-.bundle
-.cache
-.cargo
-.classpath
-.com.apple.timemachine.donotpresent
-.coverage
-.coverage.*
-.cproject
-.depend
-.deps
-.dirstamp
-.dotcloud
-.dropbox
-.dropbox-cache
-.eggs/
-.eslintcache
-.externalToolBuilders/
-.flymake*
-.fseventsd
-.fuse_hidden*
-.gdb_history
-.git
-.git/
-.gitignore
-.gitkeep
-.gopath/
-.grunt
-.hg
-.hg/
-.hgignore
-.history
-.hypothesis/
-.idea
-.idea/
-.idea_modules
-.idea_modules/
-.metals
-.mypy_cache/
-.nb-gradle/
-.netrwhist
-.node_repl_history
-.npm
-.nyc_output
-.obsidian
-.output/*
-.pc/
-.pydevproject
-.pytest_cache/
-.python-version
-.res
-.ropeproject
-.sass-cache
-.sass-cache# Python stuff
-.sqlmap_history
-.svn
-.tags
-.tags_sorted_by_file
-.venv
-.vs/
-.vscode
-.vscode/
-.webassets-cache
-.yarn
-.yarn-integrity
-CVS
-Carthage/
-Debug
-Debug/
-DerivedData
-DerivedData/
-Desktop.ini
-Gemfile.lock
-GeneratedFiles/
-GitHub.sublime-settings
-Icon
-Icon
-MANIFEST
-MANIFEST.bak
-Network Trash Folder
-Release
-Release/
-Release_MD/
-SecureROM-*
-Temporary Items
-TestResults
-Thumbs.db
-[Bb]in
-[Dd]ebug/
-[Ee]xpress
-[Oo]bj
-[Rr]elease/
-_ReSharper*
-_UpgradeReport_Files/
-__handlers__/
-__pycache__
-__pycache__/
-_build/
-_tmp.txt
-a.out
-bin/linux_mysql
-bower_components
-build
-build/
-bundles/
-cache
-cache/
-config.cache
-debug.log
-dist/
-docs/_build
-docs/_build/
-docs/_static
-docs/_templates
-makefiles
-mod_info.phph
-node_modules
-obj
-out
-output.txt
-output/
-parser.out
-project/boot/
-project/plugins/
-project/target
-release/
-release32/
-release64/
-tags
-tags-php
-target
-target/
-tmp
-tmp/
-tmp/**
-vendor/
-~$*
-~/.config/pianobar/config
-~/.gitignore
+      "$RECYCLE.BIN/"
+      "*$py.class"
+      "**/*.rs.bk"
+      "**/linux_mysql/*.d"
+      "**/target"
+      "*,cover"
+      "*-journal"
+      "*-session.json"
+      "*.Cache"
+      "*.DS_Store"
+      "*.a"
+      "*.back"
+      "*.backup"
+      "*.bak"
+      "*.cache"
+      "*.class"
+      "*.clbin"
+      "*.compiled.js"
+      "*.d"
+      "*.dSYM"
+      "*.dSYM/"
+      "*.dll"
+      "*.dmp"
+      "*.dump"
+      "*.dylib"
+      "*.elf"
+      "*.exe"
+      "*.hex"
+      "*.idb"
+      "*.kate-swp"
+      "*.kicad_pcb-bak"
+      "*.ko"
+      "*.komodoproject"
+      "*.la"
+      "*.launch"
+      "*.lib"
+      "*.lnk"
+      "*.lo"
+      "*.lock"
+      "*.log"
+      "*.manifest"
+      "*.map"
+      "*.ncb"
+      "*.o"
+      "*.o.d"
+      "*.obj"
+      "*.old"
+      "*.orig"
+      "*.out"
+      "*.pbxuser"
+      "*.pdb"
+      "*.perspectivev3"
+      "*.pid"
+      "*.pid.lock"
+      "*.py[cdo]"
+      "*.py[co]"
+      "*.py[cod]"
+      "*.pyc"
+      "*.pydevproject"
+      "*.pyo"
+      "*.py~"
+      "*.sfd~"
+      "*.so"
+      "*.so.*"
+      "*.sublime*"
+      "*.sublime-*"
+      "*.sublime-workspace"
+      "*.suit"
+      "*.suo"
+      "*.sw*"
+      "*.swn"
+      "*.swo"
+      "*.swp"
+      "*.tlh"
+      "*.tli"
+      "*.tmp"
+      "*.trace"
+      "*.un~"
+      "*.user"
+      "*.vsp"
+      "*.vspscc"
+      "*.xccheckout"
+      "*.xcodeproj/"
+      "*.xcuserstate"
+      "*.xcworkspace"
+      "*/**/.DS_Store"
+      "*_generated.phph"
+      "*~"
+      "*~.DS_STORE"
+      "*~.nib"
+      ".#*"
+      ".*.sw*"
+      ".*.swo"
+      ".*.swp"
+      ".AppleDB"
+      ".AppleDesktop"
+      ".AppleDouble"
+      ".CREATED"
+      ".DS_Store"
+      ".DocumentRevisions-V100"
+      ".LSOverride"
+      ".Python"
+      ".Spotlight-V100"
+      ".TemporaryItems"
+      ".Thumbs.db"
+      ".Trash-*"
+      ".Trashes"
+      ".VolumeIcon.icns"
+      "._*"
+      ".apdisk"
+      ".build/"
+      ".buildpath"
+      ".builds"
+      ".bundle"
+      ".cache"
+      ".cargo"
+      ".classpath"
+      ".com.apple.timemachine.donotpresent"
+      ".coverage"
+      ".coverage.*"
+      ".cproject"
+      ".depend"
+      ".deps"
+      ".dirstamp"
+      ".dotcloud"
+      ".dropbox"
+      ".dropbox-cache"
+      ".eggs/"
+      ".eslintcache"
+      ".externalToolBuilders/"
+      ".flymake*"
+      ".fseventsd"
+      ".fuse_hidden*"
+      ".gdb_history"
+      ".git"
+      ".git/"
+      ".gitignore"
+      ".gitkeep"
+      ".gopath/"
+      ".grunt"
+      ".hg"
+      ".hg/"
+      ".hgignore"
+      ".history"
+      ".hypothesis/"
+      ".idea"
+      ".idea/"
+      ".idea_modules"
+      ".idea_modules/"
+      ".metals"
+      ".mypy_cache/"
+      ".nb-gradle/"
+      ".netrwhist"
+      ".node_repl_history"
+      ".npm"
+      ".nyc_output"
+      ".obsidian"
+      ".output/*"
+      ".pc/"
+      ".pydevproject"
+      ".pytest_cache/"
+      ".python-version"
+      ".res"
+      ".ropeproject"
+      ".sass-cache"
+      ".sass-cache# Python stuff"
+      ".sqlmap_history"
+      ".svn"
+      ".tags"
+      ".tags_sorted_by_file"
+      ".venv"
+      ".vs/"
+      ".vscode"
+      ".vscode/"
+      ".webassets-cache"
+      ".yarn"
+      ".yarn-integrity"
+      "CVS"
+      "Carthage/"
+      "Debug"
+      "Debug/"
+      "DerivedData"
+      "DerivedData/"
+      "Desktop.ini"
+      "Gemfile.lock"
+      "GeneratedFiles/"
+      "GitHub.sublime-settings"
+      "Icon"
+      ''
+        Icon
+
+      ''
+      "MANIFEST"
+      "MANIFEST.bak"
+      "Network Trash Folder"
+      "Release"
+      "Release/"
+      "Release_MD/"
+      "SecureROM-*"
+      "Temporary Items"
+      "TestResults"
+      "Thumbs.db"
+      "[Bb]in"
+      "[Dd]ebug/"
+      "[Ee]xpress"
+      "[Oo]bj"
+      "[Rr]elease/"
+      "_ReSharper*"
+      "_UpgradeReport_Files/"
+      "__handlers__/"
+      "__pycache__"
+      "__pycache__/"
+      "_build/"
+      "_tmp.txt"
+      "a.out"
+      "bin/linux_mysql"
+      "bower_components"
+      "build"
+      "build/"
+      "bundles/"
+      "cache"
+      "cache/"
+      "config.cache"
+      "debug.log"
+      "dist/"
+      "docs/_build"
+      "docs/_build/"
+      "docs/_static"
+      "docs/_templates"
+      "makefiles"
+      "mod_info.phph"
+      "node_modules"
+      "obj"
+      "out"
+      "output.txt"
+      "output/"
+      "parser.out"
+      "project/boot/"
+      "project/plugins/"
+      "project/target"
+      "release/"
+      "release32/"
+      "release64/"
+      "tags"
+      "tags-php"
+      "target"
+      "target/"
+      "tmp"
+      "tmp/"
+      "tmp/**"
+      "vendor/"
+      "~$*"
+      "~/.config/pianobar/config"
+      "~/.gitignore"
 
     ];
   };
