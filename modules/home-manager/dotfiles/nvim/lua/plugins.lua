1948aa01da02a69ef2f69a4b55c7e2778ec41b67
Patrick Walsh <patrick.walsh@ironcorelabs.com>
2022-10-25 15:57:51 -0600

    post updated fixes

--- modules/home-manager/dotfiles/nvim/lua/plugins.lua
+++ modules/home-manager/dotfiles/nvim/lua/plugins.lua
@@ -624,7 +624,7 @@ M.diagnostics = function()
   local cmp_nvim_lsp = require("cmp_nvim_lsp")
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   capabilities.textDocument.completion.completionItem.snippetSupport = true
-  cmp_nvim_lsp.update_capabilities(capabilities)
+  cmp_nvim_lsp.default_capabilities(capabilities)
 
   lspconfig.rust_analyzer.setup {
     on_attach = attached,
