f014207fec527e994addb5f51b11af25e1144274
Patrick Walsh <patrick.walsh@ironcorelabs.com>
2022-11-08 09:27:06 -0700

    cachix more things

--- modules/nixpkgs.nix
+++ modules/nixpkgs.nix
@@ -17,6 +17,17 @@
       max-jobs = 8;
       cores = 0; # use them all
       allowed-users = [ "@wheel" ];
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://zmre.cachix.org"
+      ];
+
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "zmre.cachix.org-1:WIE1U2a16UyaUVr+Wind0JM6pEXBe43PQezdPKoDWLE="
+      ];
     };
     #autoOptimiseStore = true;
     #optimise.automatic = true;
