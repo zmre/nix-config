2a9f3d51749575f3936fabbe2909448279f5c6fa
Patrick Walsh <patrick.walsh@ironcorelabs.com>
2024-07-26 23:41:41 -0600

    Futz with atuin

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1263,14 +1263,16 @@ in {
   programs.atuin = {
     enable = true;
     enableZshIntegration = true;
+    flags = ["--disable-up-arrow"];
     settings = {
       update_check = false;
       search_mode = "fuzzy";
       inline_height = 33;
       common_prefix = ["sudo"];
       dialect = "us";
-      #workspaces = true;
-      #filter_mode = "directory";
+      workspaces = true;
+      filter_mode = "host";
+      filter_mode_shell_up_key_binding = "session"; # pointless now that I've disabled up arrow
       history_filter = [
         "^ "
         # "^innocuous-cmd .*--secret=.+"
